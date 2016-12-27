a91b88ff6751b050a6f9c0f8f137e647366f23e9
Abhishek Kumar <kumar.abh@husky.neu.edu>
2016-12-26 20:27:52 -0500

    Add and Delete Functionality Added

--- ProjectManager/src/App.js
+++ ProjectManager/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import Projects from './Components/Projects'
+import Projects from './Components/Projects';
+import AddProject from './Components/AddProject';
 import './App.css';
 var uniqid = require('uniqid');
 
@@ -7,24 +8,43 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      projects: [
-        {
-          id:uniqid(),
-          title: "Project 1",
-          desc: "Project 1 is about Web Development"
-        },
-        {
-          id:uniqid(),
-          title: "Project 2",
-          desc: "Project 2 is about Database Development"
-        },
-        {
-          id:uniqid(),
-          title: "Project 3",
-          desc: "Project 3 is about Alogrithm Development"
-        }
-      ]
+      projects: []
     }
+  }
+
+  componentWillMount(){
+    this.setState({projects : [
+      {
+        id:uniqid(),
+        title: "Project 1",
+        desc: "Project 1 is about Web Development"
+      },
+      {
+        id:uniqid(),
+        title: "Project 2",
+        desc: "Project 2 is about Database Development"
+      },
+      {
+        id:uniqid(),
+        title: "Project 3",
+        desc: "Project 3 is about Alogrithm Development"
+      }
+    ]});
+  }
+
+  handleAddProject(newProject){
+    //console.log(newProject)
+    let projects = this.state.projects;
+    projects.push(newProject);
+    this.setState({projects: projects});
+  }
+
+  handleDeleteProject(id){
+    //console.log(id);
+    let projects = this.state.projects;
+    let index = projects.findIndex(x => x.id === id);
+    projects.splice(index, 1);
+    this.setState({projects: projects});
   }
 
   render() {
@@ -32,7 +52,8 @@ class App extends Component {
       <div className="App">
         <div className="App-header">My First React JS Application</div>
         <br />
-        <Projects projects={this.state.projects} />
+        <AddProject addProject={this.handleAddProject.bind(this)}/>
+        <Projects projects={this.state.projects} onDelete={this.handleDeleteProject.bind(this)}/>
       </div>
     );
   }
--- ProjectManager/src/Components/ProjectItem.js
+++ ProjectManager/src/Components/ProjectItem.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 
 class ProjectItem extends Component {
+
+handleDelete(id){
+  this.props.onDelete(id);
+}
+
   render() {
     return (
       <li>
-        <strong>Project Name </strong>: {this.props.project.title}
+        <strong>Project Name </strong>: {this.props.project.title} - {this.props.project.desc} <a href="#" onClick={this.handleDelete.bind(this, this.props.project.id)}>Delete</a>
       </li>
     );
   }
